8bbf8d1e74c8a83dcf424d26698381e14b4ccab4
Luke Jiang <39086121+luke-jiang@users.noreply.github.com>
2020-05-15 21:44:58 -0400

    plus_lt

--- LF/IndProp.v
+++ LF/IndProp.v
@@ -690,7 +690,16 @@ Theorem plus_lt : forall n1 n2 m,
   n1 < m /\ n2 < m.
 Proof.
  unfold lt.
- (* FILL IN HERE *) Admitted.
+ (* FILL IN HERE *) 
+ intros. 
+  assert (H1: S n1 <= S (n1 + n2)).
+  { apply n_le_m__Sn_le_Sm. apply le_plus_l. }
+  assert (H2: S n2 <= S (n1 + n2)).
+  { apply n_le_m__Sn_le_Sm. rewrite plus_comm. apply le_plus_l. }
+  split.
+  - apply le_trans with (n:=(S (n1 + n2))); assumption.
+  - apply le_trans with (n:=(S (n1 + n2))); assumption.
+Qed.
 
 Theorem lt_S : forall n m,
   n < m ->
